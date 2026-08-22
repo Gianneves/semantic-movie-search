372c85728af08986fe063a6cc667df6b6ce47c23
Gian Neves <gianneves@hotmail.com>
2026-08-22 11:29:45 -0300

    add: upload user's avatar

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { ForbiddenException, Injectable, NotFoundException, UnprocessableEntityException } from '@nestjs/common';
+import { BadRequestException, ForbiddenException, Injectable, NotFoundException, UnprocessableEntityException } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { Users } from './entities/users.entity';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -6,6 +6,8 @@ import { CreateUserDto } from './dto/create-user.dto';
 import * as bcrypt from 'bcrypt';
 import { UpdateUserDto } from './dto/update-user.dto';
 import type { TokenPayload } from 'src/auth/utils/token-payload.interface';
+import * as path from 'path'
+import * as fs from 'fs/promises'
 
 @Injectable()
 export class UsersService {
@@ -52,17 +54,44 @@ export class UsersService {
             ...personData
         });
 
-       
+
         if (!person) {
             throw new NotFoundException('Usuário não encontrado');
         }
-       
+
         if (person.id !== tokenPayload.userId) {
             throw new ForbiddenException('Você não tem permissão para editar esse profile.');
         }
 
-
         return this.usersRepository.save(person);
+    }
 
+    async uploadPicture(file: Express.Multer.File, tokenPayload: TokenPayload) {
+        if (file.size < 1024) {
+            throw new BadRequestException('Arquivo muito grande');
+        }
+
+        const user = await this.usersRepository.findOneBy({
+            id: tokenPayload.userId
+        });
+
+        if (!user) {
+            throw new Error('Usuário não encontrado.');
+        }
+
+        const fileExtension =
+            path.extname(file.originalname)
+                .toLowerCase()
+                .substring(1);
+
+        const fileName = `${tokenPayload.userId}.${fileExtension}`;
+        const fileFullPath = path.resolve(process.cwd(), 'avatars', fileName);
+
+        await fs.writeFile(fileFullPath, file.buffer);
+
+        user.avatar = fileName;
+
+        await this.usersRepository.save(user);
+        return user;
     }
 }
